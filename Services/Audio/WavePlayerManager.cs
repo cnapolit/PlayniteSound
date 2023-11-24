d5ccfe69636a834b1cf61f00d20d8b805a19e978
Cameron Napolitano <cameronnapolitano@yahoo.com>
2023-11-24 11:41:07 -0500

    [#60] Automatically change to new default device when using WAS API

--- Services/Audio/WavePlayerManager.cs
+++ Services/Audio/WavePlayerManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 using PlayniteSounds.Models;
@@ -6,11 +8,14 @@ using PlayniteSounds.Models.Audio;
 
 namespace PlayniteSounds.Services.Audio
 {
-    public class WavePlayerManager : IWavePlayerManager
+    public class WavePlayerManager : IWavePlayerManager, IMMNotificationClient
     {
-        private readonly PlayniteSoundsSettings _settings;
+        #region Infrastructure
 
-        public MixingSampleProvider Mixer { get; private set; }
+        private readonly PlayniteSoundsSettings _settings;
+        private MMDeviceEnumerator _deviceEnumerator;
+
+        public MixingSampleProvider Mixer { get; }
         public IWavePlayer WavePlayer { get; private set; }
 
         public WavePlayerManager(PlayniteSoundsSettings settings)
@@ -20,6 +25,10 @@ namespace PlayniteSounds.Services.Audio
             Mixer = new MixingSampleProvider(waveFormat) { ReadFully = true };
             Init();
         }
+
+        #endregion
+
+        #region Implementation
 
         public void Init()
         {
@@ -36,11 +45,58 @@ namespace PlayniteSounds.Services.Audio
                 case AudioOutput.DirectSound: WavePlayer = new DirectSoundOut(); break;
                 case AudioOutput.Asio:        WavePlayer = new AsioOut();        break;
             }
+
+            if (_settings.AudioOutput is AudioOutput.Wasapi)
+            {
+                if (_deviceEnumerator is null)
+                {
+                    _deviceEnumerator = new MMDeviceEnumerator();
+                    _deviceEnumerator.RegisterEndpointNotificationCallback(this);
+                }
+            }
+            else if (_deviceEnumerator != null)
+            {
+                _deviceEnumerator.UnregisterEndpointNotificationCallback(this);
+                _deviceEnumerator.Dispose();
+                _deviceEnumerator = null;
+            }
             
             WavePlayer.Init(Mixer);
             WavePlayer.Play();
         }
 
-        public void Dispose() => WavePlayer?.Dispose();
+        public void Dispose()
+        {
+            _deviceEnumerator?.Dispose();
+            WavePlayer?.Dispose();
+        }
+
+        #region IMMNotificationClient
+
+        public void OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId) => Init();
+
+        public void OnDeviceStateChanged(string deviceId, DeviceState newState)
+        {
+            // Do Nothing
+        }
+
+        public void OnDeviceAdded(string pwstrDeviceId)
+        {
+            // Do Nothing
+        }
+
+        public void OnDeviceRemoved(string deviceId)
+        {
+            // Do Nothing
+        }
+
+        public void OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key)
+        {
+            // Do Nothing
+        }
+
+        #endregion
+
+        #endregion
     }
 }
